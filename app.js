ebd6a943c6433c0c24d613859c5e914a28bf4528
Duncan Kolba <duncankolba@gmail.com>
2014-07-29 22:03:49 +0000

    Started minimal MVC architecture

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
-var fs = require('fs')
 var http = require('http')
-var paramify = require('paramify')
+  , paramify = require('paramify')
+  , controller = require('./controller');
+
+// var props = {partials: [react.DOM.div, react.DOM.img, react.DOM.a, react.DOM.ol]};
 
 http.createServer(function (req, res) {
-  var match = paramify(req.url)
+  var match = paramify(req.url);
 
-  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.writeHead(200, {'Content-Type': 'text/html'});
 
   if (match('/*')) {
-    helloWorld(match.params, req, res)
+    controller.showIndex(req, res)
   }
 
-}).listen(8080, '127.0.0.1')
-
-function helloWorld(params, req, res) {
-  res.end('Hello World')
-}
-
-console.log('Server running at http://localhost:8080/')
+}).listen(8080);
+console.log('Server running at http://localhost:8080/');
---
+++ views.js
@@ -0,0 +1,8 @@
+var react = require('react')
+  , document = require('./components/document');
+
+module.exports.renderView = function renderView(req, res, props) {
+  var html = react.renderComponentToString(document(props));
+
+  res.end(html);
+}
