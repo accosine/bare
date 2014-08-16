a6e04a170b7346e99921f3dd1982e8588f57cdcb
Duncan Kolba <duncankolba@gmail.com>
2014-08-16 23:22:34 +0200

    Strip all things MVC from bare

--- app.js
+++ app.js
@@ -1,14 +1,1 @@
-'use strict';
-var bare = require('./lib/bare')
-  , http = require('http');
-
-bare.router.on('get', '/test', function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Testseite\n');
-  console.log('Testseite');
-});
-
-// TODO: 404 callback if route does not exist
-http.createServer(function(req, res) {
-  bare.router.dispatch(req, res, console.log);
-}).listen(8080);
+module.exports = require('./lib/bare');
